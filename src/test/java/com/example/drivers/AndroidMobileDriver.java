8cfd13de6b5dbaff618da6537fff510e2b9973e0
Timur <tim.aivarov@gmail.com>
2022-07-05 09:35:52 +0600

    add options -> full reset app/ clean cookies / add app language

--- src/test/java/com/example/drivers/AndroidMobileDriver.java
+++ src/test/java/com/example/drivers/AndroidMobileDriver.java
@@ -16,6 +16,7 @@ import java.net.URL;
 
 public class AndroidMobileDriver implements WebDriverProvider {
     private final String DEVICE_NAME = "Pixel 4 API 30";
+    private static final String LOCAL_HOST = "http://localhost:4723";
 
     @Nonnull
     @Override
@@ -26,9 +27,21 @@ public class AndroidMobileDriver implements WebDriverProvider {
         options.merge(capabilities);
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
 
+        options.clearDeviceLogsOnStart();
+
+
+        // options.setCapability("noReset", "true");
+        // options.setCapability("fullReset", "false");
+
         options.setPlatformName("Android");
         options.setDeviceName(DEVICE_NAME);
         options.setPlatformVersion("11.0");
+
+        //options.setCapability("appium:language","ru");
+        //options.setCapability("appium:locale","RU");
+
+        options.setLanguage("en");
+        options.setLocale("En");
 
         options.setApp(app.getAbsolutePath());
         options.setAppPackage("org.wikipedia.alpha");
@@ -39,7 +52,7 @@ public class AndroidMobileDriver implements WebDriverProvider {
 
     public static URL getAppiumServerUrl() {
         try {
-            return new URL("http://localhost:4723/wd/hub");
+            return new URL(LOCAL_HOST + "/wd/hub");
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -58,6 +71,4 @@ public class AndroidMobileDriver implements WebDriverProvider {
         }
         return app;
     }
-
-
 }
